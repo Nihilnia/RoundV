adac4d68396f65765f1c4a21189c4d6ca95434c5
Nihil <35506793+Nihilnia@users.noreply.github.com>
2021-10-14 02:50:53 +0300

    Add files via upload

---
+++ 20_Day/1_warmUp/Customers.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _1_warmUp
+{
+    public class Customers
+    {
+        public string CustomerID { get; set; }
+        public string ContactName { get; set; }
+        public string CompanyName { get; set; }
+        public int TotalOrders { get; set; }
+    }
+}
