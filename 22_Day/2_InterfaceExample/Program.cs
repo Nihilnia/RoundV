27f56053b294ed069f9e69f3c74931943652f56c
Nihil <35506793+Nihilnia@users.noreply.github.com>
2021-10-14 03:01:03 +0300

    Add files via upload

---
+++ 22_Day/2_InterfaceExample/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _2_InterfaceExample
+{
+    interface IPoet
+    {
+        void greet(string name);
+
+    }
+
+    class Da : IPoet
+    {
+        public void greet(string name)
+        {
+            Console.WriteLine("Hi mf," + name);
+        }
+    }
+
+    static class HelperMethods
+    {
+        public static void sayHello(string name)
+        {
+            Console.WriteLine("Hi " + name);
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            HelperMethods.sayHello("asdas");
+
+        }
+    }
+
+}
